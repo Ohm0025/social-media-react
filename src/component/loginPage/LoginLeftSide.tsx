dac04c0ccef8dad4b45a7a6c0660676122725aa3
Ohm0025 <toyohm0025@gmail.com>
2024-02-01 17:41:48 +0700

    config form login2

--- src/component/loginPage/LoginLeftSide.tsx
+++ src/component/loginPage/LoginLeftSide.tsx
@@ -2,9 +2,13 @@ import titleWeb from "/Users/apple/Desktop/westride/final project/final-project-
 
 const LoginLeftSide = () => {
   return (
-    <div className="bg-bgLogin flex-1 bg-no-repeat bg-cover bg-center min-w-[50%]">
-      <div>
+    <div className="bg-bgLogin flex-1 bg-no-repeat bg-cover bg-center min-w-[50%] flex flex-col items-center pt-[5%]">
+      <div className="">
         <img src={titleWeb} alt="title-web" />
+      </div>
+      <div className="text-white font-medium text-center mt-[-50px] flex flex-col items-center">
+        <span className="text-[32px]">Creators + Fans = Chello</span>
+        <span className="text-[18px]">#LetsGO</span>
       </div>
     </div>
   );
