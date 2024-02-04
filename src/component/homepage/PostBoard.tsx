a65f945ed3f07784b33711681dcaf39520fc3414
Ohm0025 <toyohm0025@gmail.com>
2024-02-04 07:40:43 +0700

    mod post board

--- src/component/homepage/PostBoard.tsx
+++ src/component/homepage/PostBoard.tsx
@@ -3,8 +3,14 @@ import ReactQuill from "react-quill";
 
 type MyProps = {};
 
+type BtnProps = {
+  typePostBtn: string;
+  cb: (typePost: string) => void;
+};
+
 type MyState = {
   text: string;
+  typePost: string;
 };
 
 class MyComponent extends Component<MyProps, MyState> {
@@ -12,6 +18,7 @@ class MyComponent extends Component<MyProps, MyState> {
     super(props);
     this.state = {
       text: "",
+      typePost: "only_friends",
     };
   }
 
@@ -51,6 +58,7 @@ class MyComponent extends Component<MyProps, MyState> {
           value={this.state.text}
           onChange={(value) =>
             this.setState({
+              ...this.state,
               text: value,
             })
           }
@@ -59,7 +67,38 @@ class MyComponent extends Component<MyProps, MyState> {
           theme="snow"
           modules={this.modules}
           formats={this.formats}></ReactQuill>
-        <div className="flex justify-end mt-[15px]">
+        <div className="flex justify-between items-center mt-[15px]">
+          <div className="text-[14px] font-medium flex items-center gap-[10px]">
+            <button
+              onClick={() =>
+                this.setState({ ...this.state, typePost: "public" })
+              }
+              className={`${
+                this.state.typePost === "public" ? "bg-textOne" : "bg-textFour"
+              } text-white rounded-[60px] py-1 px-3`}>
+              public
+            </button>
+            <button
+              onClick={() =>
+                this.setState({ ...this.state, typePost: "only_friends" })
+              }
+              className={`${
+                this.state.typePost === "only_friends"
+                  ? "bg-textOne"
+                  : "bg-textFour"
+              } text-white rounded-[60px] py-1 px-3`}>
+              only friends
+            </button>
+            <button
+              onClick={() =>
+                this.setState({ ...this.state, typePost: "private" })
+              }
+              className={`${
+                this.state.typePost === "private" ? "bg-textOne" : "bg-textFour"
+              } text-white rounded-[60px] py-1 px-3`}>
+              private
+            </button>
+          </div>
           <button
             onClick={() => console.log(this.state.text)}
             className="bg-textOne text-white text-[14px] font-medium px-[35px] py-[9px] rounded-[4px]">
