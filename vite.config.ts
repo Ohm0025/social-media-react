f11be0ace5be26a39f9bb72ff1964298d2c50453
Ohm0025 <toyohm0025@gmail.com>
2024-08-10 11:51:27 +0700

    add:Caddy

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/",
+//   define: {
+//     // By default, Vite doesn't include shims for NodeJS/
+//     // necessary for segment analytics lib to work
+//     global: {},
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
-  define: {
-    // By default, Vite doesn't include shims for NodeJS/
-    // necessary for segment analytics lib to work
-    global: {},
-  },
 });
